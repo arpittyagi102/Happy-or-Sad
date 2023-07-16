e6bde16e903dfa03b3fe297e3d9d8430d56e4b7d
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-16 20:28:33 +0530

    UI Change

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,12 +60,14 @@ export default function Home() {
 
   return (
     <main className={`flex min-h-screen flex-col items-center`}>
+
       <nav className="border p-7 w-full flex justify-between sm:p-2">
         <div className=" text-3xl font-bold sm:text-xl"><span className="text-green-500">HappyorSad</span>.vercel.app</div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="40px" height="40px" viewBox="0 0 32 32" version="1.1">
           <path d="M10.895 7.574c0 7.55 5.179 13.67 11.567 13.67 1.588 0 3.101-0.38 4.479-1.063-1.695 4.46-5.996 7.636-11.051 7.636-6.533 0-11.83-5.297-11.83-11.83 0-4.82 2.888-8.959 7.023-10.803-0.116 0.778-0.188 1.573-0.188 2.39z"/>
         </svg>
       </nav>
+
       <div className="text-4xl font-bold m-10 mb-5 sm:text-2xl mx-3">Free Youtube Comments Analyzer</div>
       <div className="text-2xl text-gray-500 sm:text-2xl mx-3">Get your youtube video comments analyzed in seconds</div>
       <div className="border-8 flex items-strech w-1/2 h-16 mt-10 border-green-200 sm:w-11/12">
@@ -77,9 +79,17 @@ export default function Home() {
           ></input>
           <button className="bg-green-800 h-full w-20 text-white sm:w-16" onClick={handleClick}> Go </button>
       </div>
+
+      <div className="rounded-full h-10 overflow-hidden min-w-min mt-8">
+        <div style={{width:4*result.positive+"px"}} className="h-full bg-green-500 inline-block"></div>
+        <div style={{width:4*result.neutral+"px"}} className="h-full bg-gray-400 inline-block"></div>
+        <div style={{width:4*result.negative+"px"}} className="h-full bg-red-500 inline-block"></div>
+      </div>
+
       <div className="border-8 flex items-strech w-1/2 mt-10 bg-green-200 border-green-200 text-black text-xl p-2 h-min sm:w-11/12">
         👽 This Analyzer is not 100% accurate
       </div>
+       
       <div className=" font-bold text-xl m-10 ">
         Support us : 
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 30 30" className="inline-block ms-2">
@@ -88,11 +98,6 @@ export default function Home() {
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48" className="inline-block ms-2">
           <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
         </svg>
-      </div>
-      <div className="rounded-full h-10 overflow-hidden min-w-min">
-        <div style={{width:4*result.positive+"px"}} className="h-full bg-green-500 inline-block"></div>
-        <div style={{width:4*result.neutral+"px"}} className="h-full bg-gray-400 inline-block"></div>
-        <div style={{width:4*result.negative+"px"}} className="h-full bg-red-500 inline-block"></div>
       </div>
     </main>
   );
